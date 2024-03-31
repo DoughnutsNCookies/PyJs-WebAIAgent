dad096ef611955636def31c86c3a172794319163
DoughnutsNCookies <chuahtseyung2002@gmail.com>
2024-03-31 22:26:57 +0800

    Added more utils

--- WebAI-Agent/utils.js
+++ WebAI-Agent/utils.js
@@ -10,7 +10,7 @@ const RED = "\x1b[31m";
  * @param {string} event - The name of the event to wait for.
  * @returns {Promise<void>} - A promise that resolves when the event occurs.
  */
-export async function waitForEvent(page, event) {
+export const waitForEvent = async (page, event) => {
   return page.evaluate((event) => {
     return new Promise((resolve, _) => {
       document.addEventListener(event, function () {
@@ -18,7 +18,7 @@ export async function waitForEvent(page, event) {
       });
     });
   }, event);
-}
+};
 
 /**
  * Asynchronously pauses the execution for the specified number of milliseconds.
@@ -26,13 +26,13 @@ export async function waitForEvent(page, event) {
  * @param {number} milliseconds - The number of milliseconds to pause the execution.
  * @returns {Promise<void>} - A promise that resolves after the specified number of milliseconds.
  */
-export async function sleep(milliseconds) {
+export const sleep = async (milliseconds) => {
   return await new Promise((resolve, _) => {
     setTimeout(() => {
       resolve();
     }, milliseconds);
   });
-}
+};
 
 /**
  * Converts an image file to a base64 data URI.
@@ -40,7 +40,7 @@ export async function sleep(milliseconds) {
  * @param {string} imageFile - The path to the image file.
  * @returns {Promise<string>} A promise that resolves with the base64 data URI of the image.
  */
-export async function imageToBase64(imageFile) {
+export const imageToBase64 = async (imageFile) => {
   return await new Promise((resolve, reject) => {
     fs.readFile(imageFile, (err, data) => {
       if (err) {
@@ -53,7 +53,7 @@ export async function imageToBase64(imageFile) {
       resolve(dataURI);
     });
   });
-}
+};
 
 /**
  * Prompts the user for input and returns the entered value.
@@ -61,7 +61,7 @@ export async function imageToBase64(imageFile) {
  * @param {string} text - The text to display as the prompt.
  * @returns {Promise<string>} A promise that resolves with the user's input.
  */
-export async function promptInput(text) {
+export const promptInput = async (text) => {
   let returningPrompt;
 
   const rl = readline.createInterface({
@@ -80,4 +80,41 @@ export async function promptInput(text) {
   })();
 
   return returningPrompt;
-}
+};
+
+/**
+ * Logs a message to a log file and console.
+ *
+ * @param {string} logfile - The path to the log file.
+ * @param {string} message - The message to be logged.
+ * @returns {Promise<void>} - A promise that resolves when the message is logged.
+ */
+export const log = async (logfile, message) => {
+  const rawMessage = message.replace(/\x1b\[\d{1,2}m/g, "");
+
+  fs.appendFile(logfile, rawMessage + "\n", (err) => {
+    if (err) {
+      console.error(RED + "Error writing to log file:" + err + RESET);
+    }
+  });
+  console.log(message);
+};
+
+/**
+ * Returns the current time in the format: YYYYMMDD-HHmmss.
+ *
+ * @returns {string} The current time in the format: YYYYMMDD-HHmmss.
+ */
+export const getCurrentTime = () => {
+  const padZero = (num) => (num < 10 ? "0" + num : num);
+
+  var now = new Date();
+  var year = now.getFullYear();
+  var month = padZero(now.getMonth() + 1);
+  var day = padZero(now.getDate());
+  var hours = padZero(now.getHours());
+  var minutes = padZero(now.getMinutes());
+  var seconds = padZero(now.getSeconds());
+
+  return year + month + day + "-" + hours + "h" + minutes + "m" + seconds + "s";
+};
